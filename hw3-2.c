b1bff913c9b0d6979ce274210c0dcb61883de483
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-23 12:33:36 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int s1,s2,s3,d1,d2,d3;
+scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+if(d1>s1&&d2>s2&&d3>s3)
+{
+if(s2>=d1&&s3>=d2)
+printf("1");
+else if(s3<d2&&s2<d1&&s3<d1)
+printf("3");
+else
+printf("2");
+}
+}
 
