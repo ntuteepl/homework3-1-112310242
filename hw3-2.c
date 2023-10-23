ac4107f915bef6ce9a34077a086e052411fe0433
112310242 <146065162+112310242@users.noreply.github.com>
2023-10-23 22:03:28 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,23 +3,8 @@
 
 int main()
 {
-int s1,s2,s3,d1,d2,d3,t;
+int s1,s2,s3,d1,d2,d3;
 scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-if(s1>s2){
-   t=s1;
-   s1=s2;
-   s2=t;
-}
-if(s1>s3){
-   t=s1;
-   s1=s3;
-   s3=t;
-}
-if(s2>s3){
-   t=s2;
-   s2=s3;
-   s3=t;
-}
 if(d1>s1&&d2>s2&&d3>s3)
 {
 if(s2>=d1&&s3>=d2)
@@ -30,3 +15,7 @@ else
 printf("2");
 }
 }
+
+
+
+
